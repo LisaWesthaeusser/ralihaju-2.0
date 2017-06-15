e74f7eeff3fdf6806cd47bb82d9c0d57177a1271
LisaWesthaeusser <lisawesthaeusser@gmail.com>
2017-06-15 22:33:26 +0200

    Registrieren eingerichtet

---
+++ GIB-Anamnese/src/htwg/in/gib/anam/RegistrierenServlet.java
@@ -0,0 +1,57 @@
+package htwg.in.gib.anam;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegistrierenServlet
+ */
+@WebServlet("/GIB-Anamnese/RegistrierenServlet")
+public class RegistrierenServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public RegistrierenServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String vorname = request.getParameter("Vorname");
+		String nachname = request.getParameter("Nachname");
+		String titel = request.getParameter("Titel");
+		String username = request.getParameter("Account-Name");
+		String passwort = request.getParameter("Passwort");
+
+		DatenbankAnbindung dba = new DatenbankAnbindung();
+		dba.addArzt(vorname, nachname, titel, username, passwort);
+
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+
+		String htmlResponse = "<html>";
+		htmlResponse += "<head>";
+		htmlResponse += "<body>";
+		htmlResponse += "<h2>Ihr Benutzername lautet: " + username + "</h2>";
+		htmlResponse += "<h2>Ihr Passwort lautet: " + passwort + "</h2>";
+		htmlResponse += "<h2>Ihr Benutzername lautet: " + username + "</h2>";
+		htmlResponse += "</body>";
+		htmlResponse += "</html>";
+		writer.println(htmlResponse);
+	}
+
+}
